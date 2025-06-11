fc5b92fccf55a4a90c8106ae3fc592b8312d1ccb
Bandan Chandra <bandansarker7@gmail.com>
2025-06-12 00:26:59 +0600

    Problem-2 solution

---
+++ Problem-2.c
@@ -0,0 +1,15 @@
+//Program that will decide whether a number is even or odd.
+#include<stdio.h>
+int main(){
+    int number;
+    printf("Enter the number: ");
+    scanf("%d",&number);
+    if(number%2==0){
+        printf("Even");
+    }
+
+    else{
+        printf("Odd");
+    }
+    return 0;
+}
